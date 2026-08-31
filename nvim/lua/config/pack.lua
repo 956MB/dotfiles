867b6131f8a405df66e15ece9a4d305c73bfbb07
956MB <bays@956mb.com>
2026-08-30 22:43:20 -0500

    matte-black
    * Switch to matte-black theme for a bit
    (https://github.com/tahayvr/matte-black-theme,
    https://github.com/tahayvr/matteblack.nvim). Applied to all my tool.

--- nvim/lua/config/pack.lua
+++ nvim/lua/config/pack.lua
@@ -10,6 +10,7 @@ local function rtp_add(name)
 end
 
 rtp_add 'kanso.nvim'
+rtp_add 'matteblack.nvim'
 rtp_add 'oil.nvim'
 rtp_add 'ncks.nvim'
 rtp_add 'head.nvim'
@@ -136,6 +137,8 @@ vim.pack.add {
     'https://github.com/kristijanhusak/vim-dadbod-ui',
     'https://github.com/dundalek/bloat.nvim',
     'https://github.com/stevearc/quicker.nvim',
+
+    -- Colorscheme: matteblack (local fork, submodule in custom/). Loaded in plugins/colorscheme.lua.
 }
 
 if vim.g.neovide then
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,19 @@
--- kanso.nvim is a local fork, added to rtp in pack.lua
-require('kanso').setup {
-    theme = 'zen',
-    background = {
-        dark = 'zen',
-    },
-}
-vim.cmd 'colorscheme kanso'
+-- [[ Colorscheme selection ]]
+-- One of: kanso (previous theme) or matteblack (current, local fork of tahayvr/matteblack.nvim).
+-- matteblack.nvim is a git submodule at nvim/lua/plugins/custom/matteblack.nvim, added to
+-- the rtp in config/pack.lua. Edit colors on the submodule's 'edits' branch.
+-- Uninstall: remove `rtp_add 'matteblack.nvim'` from pack.lua and deinit the submodule.
+--
+-- To switch back to kanso: comment `vim.cmd 'colorscheme matteblack'` below and
+-- uncomment the kanso block.
+-- [[ kanso.nvim (local fork, added to rtp in pack.lua) ]]
+-- require('kanso').setup {
+--     theme = 'zen',
+--     background = {
+--         dark = 'zen',
+--     },
+-- }
+-- vim.cmd 'colorscheme kanso'
+
+-- [[ matteblack.nvim (local fork via rtp, edited on the 'edits' branch) ]]
+vim.cmd 'colorscheme matteblack'
